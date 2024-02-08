e6e96c042e1ade569812c432996152e123076500
pankaj27 <patrapankaj36@gmail.com>
2024-02-08 16:55:03 +0530

    First Commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        
+    }
+}
