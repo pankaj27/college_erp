79a3dcd6a155e94ffdbe867e9818427c1f30d205
pankaj27 <patrapankaj36@gmail.com>
2024-02-08 17:02:58 +0530

    Authentication Update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -15,6 +17,13 @@ class LoginController extends Controller
     public function __invoke(LoginRequest $request)
     {
         $user = User::where('email', $request->email)->first();
-        
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+
+            throw ValidationException::withMessages([
+                'email' => ['The credentials you entered are incorrect']
+            ]);
+        }
+
     }
 }
